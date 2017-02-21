ff9bc4be1df1d2a52b1b5cbf1d4d9ff51232efd4
sb3h <470441372@qq.com>
2017-02-21 14:23:41 +0800

    避免一拉下来不能运行

--- app/src/main/java/com/template/HelloWorld.java
+++ app/src/main/java/com/template/HelloWorld.java
@@ -1,17 +1,17 @@
 package com.template;
 
-import rx.Observable;
+//import rx.Observable;
 
 /**
  * Created by sam3h on 2016/4/19 0019.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.println("123");
+        System.out.println("123");
 //        // Hello World
-        Observable.create(subscriber -> {
-            subscriber.onNext("Hello World!");
-            subscriber.onCompleted();
-        }).subscribe(System.out::println);
+//        Observable.create(subscriber -> {
+//            subscriber.onNext("Hello World!");
+//            subscriber.onCompleted();
+//        }).subscribe(System.out::println);
     }
 }
